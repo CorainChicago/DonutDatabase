c4c4035564b9417d0d884e8e8efc344531697cbd
CorainChicago <chaysmagan@gmail.com>
2015-12-29 08:48:16 -0600

    add rake command to precompile assets

--- config/application.rb
+++ config/application.rb
@@ -20,8 +20,6 @@ module ChicagoDonutClub
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 
-    # For Foundation 5
-    config.assets.precompile += %w( vendor/modernizr )
 
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
