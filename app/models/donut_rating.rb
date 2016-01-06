397512e2f17e5c9e82bba77ced08f4cc541b7286
CorainChicago <chaysmagan@gmail.com>
2016-01-06 17:27:33 -0600

    add rspec tests for user and more validation

--- app/models/donut_rating.rb
+++ app/models/donut_rating.rb
@@ -2,4 +2,7 @@ class DonutRating < ActiveRecord::Base
   belongs_to :donut
   belongs_to :user
   belongs_to :donut_shop
+
+  validates :score, :description, :presence => true
+  validates :description, :uniqueness => true
 end
