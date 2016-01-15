c94ae30b3cd10ffe91e4e1bada1558267806cdd5
CorainChicago <chaysmagan@gmail.com>
2016-01-14 22:16:46 -0600

    testing travis

--- spec/controller/user_spec.rb
+++ spec/controller/user_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe UsersController, :type => :controller do 
-   let(:user){ User.new }
+  let(:user){ User.new }
 
   describe "GET #new" do
     
@@ -16,6 +16,7 @@ RSpec.describe UsersController, :type => :controller do
     it "creates a new User when valid params are passed" do 
       expect {post :create, user: {first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, email: Faker::Internet.email, password: Faker::Lorem.word} }.to change(User,:count).by(+1)
     end
+    
     it "renders User #new when a new User lacks an email address" do 
       post :create, user: {first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, email: nil, password: Faker::Lorem.word} 
       expect(response).to render_template(:new)
