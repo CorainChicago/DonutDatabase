ba3ade1b275c2843c51417e7d78be6e871ad36c4
CorainChicago <chaysmagan@gmail.com>
2016-01-16 16:38:36 -0600

    fix donut rating form

--- app/controllers/donut_ratings_controller.rb
+++ app/controllers/donut_ratings_controller.rb
@@ -2,8 +2,18 @@ class DonutRatingsController < ApplicationController
 
 
   def create
-    byebug
-    p "hi"
+    @rating = DonutRating.new(rating_params)
+    @rating.donut_id = params[:donut_id]
+    @rating.user_id = session[:user_id]
+    @donut = Donut.find(@rating.donut_id)
+    @rating.donut_shop_id = @donut.donut_shop_id
+    if @rating.valid?
+      @rating.save
+      redirect_to @donut
+    else
+      @errors = @rating.errors.full_messages
+      render 'donut_shops/new'
+    end
   end
 
   private
@@ -11,5 +21,6 @@ class DonutRatingsController < ApplicationController
   def rating_params
     params.require(:donut_rating).permit( :score, :description)
   end
+
 
 end
