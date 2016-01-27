99fd1a54882f00964caac61cb8615bb174d61937
CorainChicago <chaysmagan@gmail.com>
2016-01-27 07:34:46 -0600

    delete extra words from new donut form, just random words

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -1,22 +1,28 @@
-// var address = "1600 Amphitheatre Parkway, Mountain  View";
-// var geocoder = new google.maps.Geocoder();
-// geocoder.getLatLng(address, function(point) {
-//        var latitude = point.y;
-//        var longitude = point.x;  
+var map;
+var latitude;
+var longitude
 
-//       function initMap() {
-//         var map = new google.maps.Map(document.getElementById('map'), {
-//           zoom: 10,
-//           center: {lat: -34.397, lng: 150.644}
-//         });
-//         var geocoder = new google.maps.Geocoder();
+function initMap() {
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: latitude, lng: longitude},
+    zoom: 8
+  });
+}
+    
+    
+function othercodeAddress(){
+  var result = $.ajax({
+    url: 'https://maps.googleapis.com/maps/api/geocode/json?address=1246+Adams+Street,+Franklin+TN&key=AIzaSyABDLpDWA8NZkvbfuP7RCIWjL7_UrLsWmw',
+    method: "GET"
+})
+  result.done(function(response){
+    latitude = response.results[0].geometry.location.lat;
+    longitude = response.results[0].geometry.location.lng;
+  });
+}
 
-//         document.getElementById('submit').addEventListener('click', function() {
-//           geocodeAddress(geocoder, map);
-//         });
-//       }
-  
-// });
+
+
 
 
 
