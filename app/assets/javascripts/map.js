604c9bd15cbc97349942b2ca99030421dc731d99
CorainChicago <chaysmagan@gmail.com>
2016-02-04 14:49:06 -0600

    removed unnecessary files

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -1,6 +1,6 @@
-// var map;
-// var latitude;
-// var longitude
+var map;
+var latitude;
+var longitude
 
 // function initMap() {
 //   map = new google.maps.Map(document.getElementById('map'), {
@@ -8,18 +8,26 @@
 //     zoom: 8
 //   });
 // }
+
+var map;
+function initMap() {
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: -34.397, lng: 150.644},
+    zoom: 8
+  });
+}
     
     
-// function othercodeAddress(){
-//   var result = $.ajax({
-//     url: 'https://maps.googleapis.com/maps/api/geocode/json?address=1246+Adams+Street,+Franklin+TN&key=AIzaSyABDLpDWA8NZkvbfuP7RCIWjL7_UrLsWmw',
-//     method: "GET"
-// })
-//   result.done(function(response){
-//     latitude = response.results[0].geometry.location.lat;
-//     longitude = response.results[0].geometry.location.lng;
-//   });
-// }
+function othercodeAddress(){
+  var result = $.ajax({
+    url: 'https://maps.googleapis.com/maps/api/geocode/json?address=1246+Adams+Street,+Franklin+TN&key=AIzaSyABDLpDWA8NZkvbfuP7RCIWjL7_UrLsWmw',
+    method: "GET"
+})
+  result.done(function(response){
+    latitude = response.results[0].geometry.location.lat;
+    longitude = response.results[0].geometry.location.lng;
+  });
+}
 
 
 
