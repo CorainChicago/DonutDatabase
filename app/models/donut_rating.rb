f47ad471a92488f2e0fa1d58a6b77c4ea6b2d145
CorainChicago <chaysmagan@gmail.com>
2016-06-08 09:01:21 -0500

    add average method to donut rating

--- app/models/donut_rating.rb
+++ app/models/donut_rating.rb
@@ -1,10 +1,13 @@
 class DonutRating < ActiveRecord::Base
-  belongs_to :donut
-  belongs_to :user
-  belongs_to :donut_shop
+  validates :user_id, :donut_id, :score, :description, :donut_shop_id, :presence => true
 
-  validates :score, :description, :user_id, :donut_shop_id, presence: true
+  def self.average_rating(donut_id)
+    ratings = where(donut_id: donut_id)
+    sum(ratings) / ratings.length
+  end
 
- 
-
+  private
+  def self.sum(ratings)
+    ratings.inject (0) {|sum, r| sum + r.score } 
+  end
 end
