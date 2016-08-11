1d7843b570c9d5e48ab1ef11baecf863341027f3
CorainChicago <chaysmagan@gmail.com>
2016-08-11 10:07:08 -0500

    add spec to cover lines 10-11 of DonutRatings controller

--- spec/controller/donut_rating_spec.rb
+++ spec/controller/donut_rating_spec.rb
@@ -8,6 +8,16 @@ RSpec.describe DonutRatingsController, :type => :controller do
         rating = { score: 5, description: Faker::Lorem.sentence, donut_shop_id: 1, donut_id: 1, user_id: 1 } 
         expect{DonutRating.create(rating)}.to change {DonutRating.count}.from(0).to 1 
       end
+      it "redirects to the donut" do
+         user = User.create(first_name: "Charlie", last_name: "Cat", email: "test@cat.com", password: "password")
+        session[:user_id] = user.id
+        donut = Donut.create!(name: Faker::Lorem.word, occasion: "Splurge", description: Faker::Lorem.sentence, type_of_donut: "Yeast", donut_shop_id: 1) 
+        rating = { score: 5, description: Faker::Lorem.sentence, donut_shop_id: 1, donut_id: donut.id, user_id: 1 } 
+        post :create, { :donut_rating => rating, :donut_id => donut.id, :user_id => user.id } 
+
+        # expect(response).to be_successful
+        expect(response).to redirect_to("/donuts/#{assigns(:donut).id}")
+      end
     end
 
     context "when invalid params are passed" do
