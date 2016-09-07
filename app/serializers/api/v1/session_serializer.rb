4fe643d7c270e597feb5ea46b0ad87b65170e407
CorainChicago <chaysmagan@gmail.com>
2016-09-07 08:59:11 -0500

    create the session serializer with the needed attributes for the user

--- app/serializers/api/v1/session_serializer.rb
+++ app/serializers/api/v1/session_serializer.rb
@@ -1,3 +1,3 @@
-class SessionSerializer < ActiveModel::Serializer
-  attributes :id
+class Api::V1::SessionSerializer < ActiveModel::Serializer
+  attributes :id, :email, :first_name, :last_name, :authentication_token
 end
